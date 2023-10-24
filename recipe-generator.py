bf83c65f7651aa5f46000d1cfa802a0fd386f985
TimothyJNeale <tim.neale@zenbyte.ai>
2023-10-24 11:51:21 +0100

    Recipe generating prompt completed

--- recipe-generator.py
+++ recipe-generator.py
@@ -13,10 +13,25 @@ openai.api_key = os.environ["OPENAI_API_KEY"]
 
 # Create the prompt for the recipe
 def create_recipe_prompt(ingredients):
-    prompt = f"""Create a detailed recipe based on only the following ingredients:
+    list_of_ingredients = '\n    #      '.join(ingredients)
+    prompt = f"""
+    # Create a detailed recipe based only on the following listed ingredients.
+    # Additionally, assign a title starting with 'Recipe Title' to the recipe.
+    #
+    # Recipe Title:
     #
     # Ingredients:
-    # {', '.join(ingredients)}
+    #      {list_of_ingredients}
     #
     # Instructions:"""
     return prompt
+
+prompt = create_recipe_prompt(['chicken', 'rice', 'broccoli'])
+print
+
+response = openai.Completion.create(engine="text-davinci-003",
+                                    prompt=prompt,
+                                    max_tokens=256,
+                                    temperature=0.8)
+
+print(response['choices'][0]['text'])
