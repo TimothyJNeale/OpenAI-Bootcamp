982c96111ba8510b21c5200e18880de18de38295
Tim Neale <119433369+TimothyJNeale@users.noreply.github.com>
2023-10-30 19:57:46 +0000

    Added function to format and write html page

--- post-creator.py
+++ post-creator.py
@@ -1,5 +1,6 @@
 import openai
 import os
+import shutil
 
 from git import Repo
 
@@ -26,14 +27,39 @@ def updae_blog(commit_message='Update blog'):
     origin = repo.remote(name='origin')
     origin.push()
 
-random_text_string = "werwerwerwerwerwerwefdkdvjdni8bbc"
+def create_post(title, content, cover_image=None):
+    cover_image = Path(cover_image)
 
-# Write random text string to index.html
-with open(PATH_TO_BLOG/"index.html", "w") as f:
-    f.write(random_text_string)
+    files = len(list(PATH_TO_CONTENT.glob("*.html")))
+    new_title = f"{files+1}.html"
+    path_to_new_post = PATH_TO_CONTENT/new_title
 
-# Update blog
-updae_blog()
+    shutil.copy(cover_image, PATH_TO_CONTENT)
+
+    if not os.path.exists(path_to_new_post):
+        with open(path_to_new_post, "w") as f:
+            f.write('<DOCTYPE html>\n')
+            f.write('<html>\n')
+            f.write('<head>\n')
+            f.write('<meta charset="utf-8">\n')
+            f.write('<meta name="viewport" content="width=device-width, initial-scale=1">\n')
+            f.write(f'<title>{title}</title>\n')
+            f.write('</head>\n')
+            f.write('<body>\n')
+            f.write(f"<img src='{cover_image.name}' alt='Cover Image'> <br /> \n")
+            f.write(f'<h1>{title}</h1>\n')
+            f.write(content.replace('\n', '<br /> \n'))
+            f.write('</body>\n')
+            f.write('</html>\n')
+
+            print(f"Created new post: {path_to_new_post}")
+            return path_to_new_post
+        
+    else:
+        raise FileExistsError("File already exists")
+
+
+path_to_new_post = create_post("Test Post", "This is a test post", cover_image="dev/Engineering.png")
 
 
 
