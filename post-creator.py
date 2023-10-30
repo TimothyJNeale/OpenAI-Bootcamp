77e627c09d7edb37b81df8755448f4c3ade8b175
Tim Neale <119433369+TimothyJNeale@users.noreply.github.com>
2023-10-30 19:10:00 +0000

    Now can update blog on github pages

--- post-creator.py
+++ post-creator.py
@@ -1,20 +1,42 @@
 import openai
-from dotenv import load_dotenv
 import os
 
+from git import Repo
+
+from pathlib import Path
+WORKING_PATH = "/Users/sealislandmedia/Desktop/TimothyJNeale.github.io"
+
+PATH_TO_BLOG_REPO = Path(os.path.join(WORKING_PATH, ".git"))
+PATH_TO_BLOG = PATH_TO_BLOG_REPO.parent
+
+PATH_TO_CONTENT = PATH_TO_BLOG/"content"
+PATH_TO_CONTENT.mkdir(exist_ok=True, parents=True)
+
 # load environment variables from .env file
+from dotenv import load_dotenv
 load_dotenv()
+
+
+def updae_blog(commit_message='Update blog'):
+    repo = Repo(PATH_TO_BLOG_REPO)
+
+    repo.git.add(all=True)
+    repo.index.commit(commit_message)
+
+    origin = repo.remote(name='origin')
+    origin.push()
+
+random_text_string = "werwerwerwerwerwerwefdkdvjdni8bbc"
+
+# Write random text string to index.html
+with open(PATH_TO_BLOG/"index.html", "w") as f:
+    f.write(random_text_string)
+
+# Update blog
+updae_blog()
+
+
 
 # get api key from environment variable
 api_key = os.environ["OPENAI_API_KEY"]
-print(api_key)
-
 openai.api_key = api_key
-#print(openai.Engine.list())
-
-response = openai.Completion.create(
-    model='gpt-3.5-turbo-instruct',
-    prompt="Give me two reasons to learn OpenAI API with pythton ",
-    max_tokens=300)
-
-print(response['choices'][0]['text'])
