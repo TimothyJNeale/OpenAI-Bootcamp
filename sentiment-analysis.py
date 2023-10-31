1ced0775e7a49d148d6d7ae67d9428d3415287d1
Tim Neale <119433369+TimothyJNeale@users.noreply.github.com>
2023-10-31 15:31:13 +0000

    Added sentiment-analysis.py

---
+++ sentiment-analysis.py
@@ -0,0 +1,12 @@
+import openai
+from dotenv import load_dotenv
+import os
+
+# load environment variables from .env file
+load_dotenv()
+
+# get api key from environment variable
+api_key = os.environ["OPENAI_API_KEY"]
+print(api_key)
+
+openai.api_key = api_key
