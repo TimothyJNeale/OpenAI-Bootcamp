49acee791ef9bcaef1055a68d2142420b3638285
Tim Neale <119433369+TimothyJNeale@users.noreply.github.com>
2023-10-31 15:40:59 +0000

    Configured connection to Reddit

--- sentiment-analysis.py
+++ sentiment-analysis.py
@@ -1,10 +1,19 @@
 import openai
 from dotenv import load_dotenv
 import os
+import praw
 
 # load environment variables from .env file
 load_dotenv()
 
+reddit = praw.Reddit(client_id=os.environ["REDDIT_CLIENT_ID"],
+                     client_secret=os.environ["REDDIT_CLIENT_SECRET"],
+                     user_agent=os.environ["REDDIT_USER_AGENT"]
+                     )
+
+for submision in reddit.subreddit("finance").hot(limit=5):
+    print(submision.title)
+                                    
 # get api key from environment variable
 api_key = os.environ["OPENAI_API_KEY"]
 print(api_key)
