50333eea06cdcd9255ab878b6bff0a46fe6a664f
Tim Neale <119433369+TimothyJNeale@users.noreply.github.com>
2023-11-06 11:22:18 +0000

    Example of accessing comments on Reddit post

--- sentiment-analysis.py
+++ sentiment-analysis.py
@@ -11,11 +11,26 @@ reddit = praw.Reddit(client_id=os.environ["REDDIT_CLIENT_ID"],
                      user_agent=os.environ["REDDIT_USER_AGENT"]
                      )
 
-for submision in reddit.subreddit("finance").hot(limit=5):
-    print(submision.title)
-                                    
-# get api key from environment variable
-api_key = os.environ["OPENAI_API_KEY"]
-print(api_key)
+subreddit_stocks = reddit.subreddit("stocks")
+print(subreddit_stocks.display_name)
+print(subreddit_stocks.title)
+#print(subreddit_stocks.description)
+print(subreddit_stocks.accounts_active)
 
-openai.api_key = api_key
+for posts in subreddit_stocks.hot(limit=5):    
+    print(posts.title)
+    submission = reddit.submission(id=posts.id)
+    # print top 2 comments per submission
+    counter = 0
+    for top_level_comment in submission.comments:
+        if top_level_comment != '[deleted]':
+            print(top_level_comment.body)
+            counter += 1
+            if counter == 2:
+                break
+                                    
+# # get api key from environment variable
+# api_key = os.environ["OPENAI_API_KEY"]
+# print(api_key)
+
+# openai.api_key = api_key
