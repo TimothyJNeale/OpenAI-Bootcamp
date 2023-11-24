1a2c123114a143a47929d2a07d6103010e78f03c
Tim Neale <119433369+TimothyJNeale@users.noreply.github.com>
2023-11-24 11:33:51 +0000

    Added hallucination module

---
+++ hallucinations.py
@@ -0,0 +1,72 @@
+# Examples of hallucinations and how to avoid them 
+
+######################################### IMPORTS #############################################
+import openai
+import logging
+import os
+
+from dotenv import load_dotenv
+
+######################################## CONSTANTS ############################################
+
+
+DATA_DIRECTORY ='data'
+
+
+########################################### DATA ##############################################
+
+# load environment variables from .env file
+load_dotenv()
+
+
+##################################### HELPER FUCTIONS #########################################
+
+# Use chat completion
+def get_chat_completion(prompt, model="gpt-3.5-turbo", temperature=0):
+    messages = [{"role": "user", "content": prompt}]
+    response = openai.ChatCompletion.create(
+        model=model,
+        messages=messages,
+        temperature=temperature
+    )
+    return response.choices[0].message["content"]
+
+# Standard completion
+def get_completion(prompt, model="gpt-3.5-turbo-instruct", temperature=0, max_tokens=300, stop="\"\"\""):
+    response = openai.Completion.create(
+        model=model,
+        prompt=prompt,
+        temperature=temperature,
+        max_tokens=max_tokens,
+        stop=stop)
+
+    return response.choices[0].text
+
+######################################## LOGGING ##############################################
+
+logging.basicConfig(level=logging.INFO, format=' %(asctime)s - %(levelname)s - %(message)s')
+logging.disable(logging.DEBUG) # Supress debugging output from modules imported
+#logging.disable(logging.CRITICAL) # Uncomment to disable all logging
+
+
+######################################### START ###############################################
+logging.info('Start of program')
+
+# Get the current DATA directory
+home = os.getcwd()
+data_dir = os.path.join(home, DATA_DIRECTORY)
+logging.info(data_dir)
+
+os.chdir(data_dir)
+
+# Authenticate with OpenAI                             
+api_key = os.environ["OPENAI_API_KEY"]
+openai.api_key = api_key
+
+########################################## MAIN ###############################################
+logging.info('Main section entered')
+
+
+
+######################################### FINISH ##############################################
+logging.info('End of program')
